d576e3ab13ca63a649c7499775f34c8e19b19b84
Simon Støvring <mail@simonbs.dk>
2020-01-30 00:15:21 +0100

    Clean up

--- DarkDocs/AppDelegate.swift
+++ DarkDocs/AppDelegate.swift
@@ -10,13 +10,7 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {}
     
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-    
+    func applicationWillTerminate(_ aNotification: Notification) {}
 }
